273226396b9bdd798bab65b7bf6435cb7ee7e604
Humberto <humbhenri@gmail.com>
2019-12-19 15:14:12 -0300

    mark todo as done

--- src/todo.service.js
+++ src/todo.service.js
@@ -1,11 +1,14 @@
 app.service('TodoService', function() {
     this.save = todo => {
         this.todos = this.todos || [];
-        this.todos.push(todo);
+        this.todos.push({ text: todo, id: this.nextId() });
         return Promise.resolve();
     };
 
     this.list = () => {
         return Promise.resolve(this.todos);
     };
+
+    this.id = 0;
+    this.nextId = () => this.id++;
 });
